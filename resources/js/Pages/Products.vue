56f7b8f2c924c52618d1cd71126f3f2fe55efaed
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-22 15:33:43 +0800

    Update Products.vue
    update product form, and filter form,

--- resources/js/Pages/Products.vue
+++ resources/js/Pages/Products.vue
@@ -39,7 +39,7 @@ function update(){
 }
 
 function remove(id){
-    if (confirm("Do you want to remove this schedule?") == true) {
+    if (confirm("Do you want to remove this product?") == true) {
         router.delete('/delete-product/' + id, {
             onSuccess: () => {
             },
@@ -82,51 +82,38 @@ console.log(props.category)
 </script>
 <template>
     <div class="d-flex justify-center">
-        <v-card class="w-100">
+        <v-card title="Products" subtitle="List of products" class="w-100">
            <v-container fluid>
-                <v-card-title>
-                    Products
-                </v-card-title>
                 <v-card-text>
                     <div class="container">
                         <v-btn color="success" href="/create-product" class="mb-5" prepend-icon="mdi-plus-circle">Create</v-btn>
-                            
-                            <v-row class="justify-space-between">
-                                <v-col
-                                    cols="12"
-                                    md="5"
-                                    sm="6"
-                                >
-                                    <v-text-field
-                                        v-model="search"
-                                        append-icon="search"
-                                        label="Search"
+                        <v-row class="justify-space-between">
+                            <v-col cols="12" md="5" sm="6">
+                                <v-text-field
+                                    v-model="search"
+                                    append-icon="search"
+                                    label="Search"
+                                    variant="outlined"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="3" sm="4">
+                                <v-form @submit.prevent="applyFilter()">
+                                    <v-select 
+                                        v-model="form.filter" 
+                                        label="Filter"
                                         variant="outlined"
-                                    ></v-text-field>
-                                </v-col>
-                                <v-col
-                                    cols="12"
-                                    md="3"
-                                    sm="4"
-                                >
-                                    <v-form @submit.prevent="applyFilter()">
-                                        <v-select 
-                                            v-model="form.filter" 
-                                            label="Filter"
-                                            variant="outlined"
-                                            :items="props.category"
-                                            item-title="name"
-                                            item-value="name"
-                                            chips
-                                        ></v-select>
-                                        <div class="d-flex justify-end mb-2">
-                                            <v-btn type="submit" class="mx-2">Apply Filters</v-btn>
-                                            <v-btn href="/products">Remove Filters</v-btn>
-                                        </div>
-                                    </v-form>
-                                </v-col>
-                                
-                            </v-row>
+                                        :items="props.category"
+                                        item-title="name"
+                                        item-value="name"
+                                        chips
+                                    ></v-select>
+                                    <div class="d-flex justify-end mb-2">
+                                        <v-btn type="submit" class="mx-2">Apply Filters</v-btn>
+                                        <v-btn href="/products">Remove Filters</v-btn>
+                                    </div>
+                                </v-form>
+                            </v-col>   
+                        </v-row>
                     </div>
                     <v-table>
                         <thead>
@@ -159,16 +146,17 @@ console.log(props.category)
                                 <td>{{ item.description }}</td>
                                 <td>{{ item.date_time }}</td>
                                 <td>
-                                    <v-btn icon="mdi-pencil" @click="openDialog(item)" class="ma-2 pa-2" size="x-small"></v-btn>
-                                    <v-btn icon="mdi-trash-can-outline" @click="remove(item.id)" class="ma-2 pa-2" size="x-small"></v-btn>
+                                    <v-row class="d-flex justify-space-between">
+                                        <v-btn icon="mdi-pencil" @click="openDialog(item)" size="x-small"></v-btn> 
+                                        <v-btn icon="mdi-trash-can-outline" @click="remove(item.id)" size="x-small"></v-btn>
+                                    </v-row>
                                 </td>
-                                
                             </tr>
-        
-                        </tbody>
-                        
+                        </tbody>   
                     </v-table>
+
                     <div v-if="products.data.length == 0" class="text-center text-h3">No Item</div>
+
                     <v-pagination 
                         v-model="props.products.current_page" 
                         :length="props.products.last_page" 
@@ -181,50 +169,42 @@ console.log(props.category)
                     >
                         <v-card>
                             <v-container fluid>
-                                    <v-card-title>
-                                        Update Product
-                                    </v-card-title>
-                                    <v-form @submit.prevent="update()">
-                                        <v-card-text>   
-                                                <v-text-field 
-                                                    v-model="form.name" 
-                                                    label="Name"
-                                                    :error-messages="$page.props.errors.name"
-                                                    variant="outlined"
-                                                ></v-text-field>
-                                                <v-select
-                                                    :items="items = 
-                                                            [
-                                                                'Category 1',
-                                                                'Category 2',
-                                                                'Category 3',
-                                                                'Category 4',
-                                                            ]"
-                                                    v-model="form.category" 
-                                                    label="Category"
-                                                    :error-messages="$page.props.errors.category"
-                                                    variant="outlined"
-                                                ></v-select>
-                                                <v-textarea
-                                                    v-model="form.description" 
-                                                    label="Description"
-                                                    :error-messages="$page.props.errors.description"
-                                                    variant="outlined"
-                                                ></v-textarea>
-                                                <v-text-field
-                                                    v-model="form.date_time" 
-                                                    type="datetime-local" 
-                                                    label="Date & Time"
-                                                    :error-messages="$page.props.errors.date_time"
-                                                    variant="outlined"
-                                                ></v-text-field>
-                                            
-                                        </v-card-text>
-                                        <v-card-actions class="justify-end">
-                                            <v-btn color="primary" @click="dialog = false">Close</v-btn>
-                                            <v-btn type="submit">Update</v-btn>
-                                        </v-card-actions>
-                                    </v-form>
+                                <v-card-title>
+                                    Update Product
+                                </v-card-title>
+                                <v-form @submit.prevent="update()">
+                                    <v-card-text>   
+                                        <v-text-field 
+                                            v-model="form.name" 
+                                            label="Name"
+                                            :error-messages="$page.props.errors.name"
+                                            variant="outlined"
+                                        ></v-text-field>
+                                        <v-text-field 
+                                            v-model="form.category" 
+                                            label="Category"
+                                            :error-messages="$page.props.errors.category"
+                                            variant="outlined"
+                                        ></v-text-field >
+                                        <v-textarea
+                                            v-model="form.description" 
+                                            label="Description"
+                                            :error-messages="$page.props.errors.description"
+                                            variant="outlined"
+                                        ></v-textarea>
+                                        <v-text-field
+                                            v-model="form.date_time" 
+                                            type="datetime-local" 
+                                            label="Date & Time"
+                                            :error-messages="$page.props.errors.date_time"
+                                            variant="outlined"
+                                        ></v-text-field>    
+                                    </v-card-text>
+                                    <v-card-actions class="justify-end">
+                                        <v-btn color="primary" @click="dialog = false">Close</v-btn>
+                                        <v-btn type="submit">Update</v-btn>
+                                    </v-card-actions>
+                                </v-form>
                             </v-container>
                         </v-card>
                     </v-dialog>
